51029696e223a42db2e5ebc1888d464bb95b925d
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-21 21:44:22 -0500

    SearchForm

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,5 +1,29 @@
 const SearchForm = () => {
-  return <h2>Search Form</h2>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const searchValue = event.target.elements.search.value;
+    if (!searchValue) {
+      return;
+    }
+    console.log(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
