b3318996d6f5d8c1217ec0e8e7c1fdfbf6e7837f
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-23 22:34:43 -0500

    Setup React Query

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AppProvider } from './context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </AppProvider>
   </React.StrictMode>
 );
