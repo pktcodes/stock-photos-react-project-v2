4e78381ff2078f18b3d556f9ab43e9bc001242c1
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-23 23:16:41 -0500

    ENV Variables

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,3 +1,9 @@
+const url = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_ACCESS_KEY
+}&query=cat`;
+
+console.log(url);
+
 const Gallery = () => {
   return <h2>Gallery</h2>;
 };
