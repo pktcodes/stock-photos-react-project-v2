6867ba28a29588b14d0f56fdeb6ef458b3f34be2
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-23 23:33:30 -0500

    Setup Query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,10 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
 const url = `https://api.unsplash.com/search/photos?client_id=${
   import.meta.env.VITE_ACCESS_KEY
 }&query=cat`;
 
-console.log(url);
-
 const Gallery = () => {
+  const result = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const response = await axios.get(url);
+      return response.data;
+    },
+  });
+  console.log(result);
+
   return <h2>Gallery</h2>;
 };
 
