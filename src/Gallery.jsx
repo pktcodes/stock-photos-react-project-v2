890ef35c5fe316d319c3981aafd9d9a61d28815c
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-24 00:00:20 -0500

    Render Images

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -13,9 +13,42 @@ const Gallery = () => {
       return response.data;
     },
   });
-  console.log(result);
 
-  return <h2>Gallery</h2>;
+  if (result.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>loading...</h4>
+      </section>
+    );
+  }
+
+  if (result.isError) {
+    return (
+      <section className="image-container">
+        <h4>there was an error...</h4>
+      </section>
+    );
+  }
+
+  const data = result.data.results;
+
+  if (data.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>no results found</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className="image-container">
+      {data.map((image) => {
+        const { id, alt_description } = image;
+        const url = image?.urls.regular;
+        return <img key={id} src={url} alt={alt_description} className="img" />;
+      })}
+    </section>
+  );
 };
 
 export default Gallery;
